1d628edcd6b532a51f50e3ab2be066ca401b4412
Paul Tonning <paultonning@ac5Teacher.cis>
2018-05-10 14:47:55 -0700

    Removed comments

--- imageview.cpp
+++ imageview.cpp
@@ -21,25 +21,6 @@ ImageView::ImageView(QWidget *parent) :
 
 void ImageView::mousePressEvent(QMouseEvent * e)
 {
-    /*
-    auto pos = mapToScene(e->pos()); // This maps "real world" coordinates to the Scene coordinates
-
-    scene->addEllipse(pos.x(),pos.y(),		//x,y position of the upper left hand corner
-                      10,10,				// width and height of the rectangle
-                      QPen(Qt::black),		// the pen used for the outline
-                      QBrush(Qt::black)); 	// the brush used for the inside of the ellipse
-    if(last_position.x() != 0 && last_position.y() != 0)
-    {
-        scene->addLine(last_position.x(),
-                       last_position.y(),
-                        pos.x(),
-                        pos.y(),
-                        QPen(Qt::black)
-                       );
-    }
-    last_position = pos;
-    //qDebug() << e->pos();
-    */
     auto pos = mapToScene(e->pos()); // This maps "real world" coordinates to the Scene coordinates
     origin = e->pos();
     if (!rubberBand)
