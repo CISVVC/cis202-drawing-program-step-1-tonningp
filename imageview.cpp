a2be970b8314413f3f10b1942da23995048086bd
Paul Tonning <paultonning@ac5Teacher.cis>
2018-05-10 15:47:05 -0700

    Added the ellipse adding code.

--- imageview.cpp
+++ imageview.cpp
@@ -21,21 +21,13 @@ ImageView::ImageView(QWidget *parent) :
 
 void ImageView::mousePressEvent(QMouseEvent * e)
 {
-    auto pos = mapToScene(e->pos()); // This maps "real world" coordinates to the Scene coordinates
     origin = e->pos();
     if (!rubberBand)
-            rubberBand = new QRubberBand(QRubberBand::Rectangle, this);
+            rubberBand = new QRubberBand(QRubberBand::Line, this);
     rubberBand->setGeometry(QRect(origin, QSize()));
-    rubberBand->show();
+    if(current_op != Operation::noop)
+        rubberBand->show();
 
-//    if(current_op == Operation::ellipse )
-//    {
-//        scene->addEllipse(pos.x(),pos.y(),		//x,y position of the upper left hand corner
-//                  100,100,				// width and height of the rectangle
-//                  QPen(Qt::blue),		// the pen used for the outline
-//                  QBrush(Qt::blue)); 	// the brush used for the inside of the ellipse
-//        last_position = pos;
-//    }
 }
 
 void ImageView::mouseReleaseEvent(QMouseEvent *event)
@@ -43,19 +35,35 @@ void ImageView::mouseReleaseEvent(QMouseEvent *event)
    rubberBand->hide();
    // determine selection, for example using QRect::intersects()
    // and QRect::contains().
+   auto pos = mapToScene(origin); // This maps "real world" coordinates to the Scene coordinates
+   if(current_op == Operation::ellipse )
+   {
+        QGraphicsEllipseItem *ellipse = scene->addEllipse(pos.x(),pos.y(),		//x,y position of the upper left hand corner
+                  rubberBand->width(),rubberBand->height(),				// width and height of the rectangle
+                  QPen(Qt::blue),		// the pen used for the outline
+                  QBrush(Qt::blue)); 	// the brush used for the inside of the ellipse
+        ellipse->setFlag(QGraphicsItem::ItemIsMovable);
+        //ellipse->setFlag(QGraphicsItem::ItemIsSelectable);
+        //last_position = pos;
+   }
 }
 
 void ImageView::mouseMoveEvent(QMouseEvent *event)
 {
-    rubberBand->setGeometry(QRect(origin, event->pos()).normalized());
+    if(current_op != Operation::noop)
+        rubberBand->setGeometry(QRect(origin, event->pos()).normalized());
+    QGraphicsView::mouseMoveEvent(event);
 }
 
 void ImageView::addingEllipse(bool toggled)
 {
-    qDebug()  << "ImageView::addingEllipse";
     if(toggled)
     {
         current_op = Operation::ellipse;
+    }
+    else
+    {
+       current_op = Operation::noop;
     }
 }
 
